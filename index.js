bc6ab2b90f385eb7de57627017257bc438167b0a
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2023-02-01 15:21:35 -0600

    adds more questions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let title = response.title
+
 
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs')
+const { default: Choices } = require('inquirer/lib/objects/choices');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 inquirer
@@ -12,6 +14,21 @@ inquirer
             name: 'title',
             //  validate: titleInput = title ? console.log('Please enter your project title!')
         },
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },// needs validation
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your username?',
+            name: 'username'
+        },// needs validation
         {
             type: 'input',
             message: 'What was your motivation?  Why did you build this project?',
@@ -27,11 +44,44 @@ inquirer
             message: 'What did you learn?',
             name: 'description3'
         },
+        {
+            type: 'instalation',
+            message: 'What are the steps required to install your project?  Provide a step-by-step description of how to get the development enviroment running.',
+            name: 'instalation'
+        },
+        {
+            type: 'input',
+            message: 'What is your project used for and how do you use it?',
+            name: 'usage'
+        },//validate
+        // best way to capture input for collaborators and if there are collaborators{
+        //     type: 'credits',
+        //     message: '',
+        //     name: 'credits'
+        // },// also can you run an inquirerer inside of an inquirerer?
+        {
+            type: 'checkbox',
+            message: 'What licenses are pertinent?',
+            name: 'licenses',
+            choices: ['Apache 2.0 License', 'BSD License', 'CeCILL License 2.1',  
+            'Eclipse Public License', 'GNU', 'IBM', 'Mit', 'Mozilla', 'JAM', 'Python']
+        }, {
+            type: 'input',
+            message: 'what test can be run to see how the project works??',
+            name: 'tests'
+        },
+        
+            //ADD badges
 
     ])
     .then((response) => {
-        let title = response.title
-        console.log(title)
+        let title = response.title;
+        console.log(title);
+        console.log(response);
+        return
+
+
+
     })
 
     ;
